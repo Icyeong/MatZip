3ecfb51d98822211754715eb26ebd37c1472846c
icyeong <dbcjswjs@naver.com>
2024-09-18 02:58:33 +0900

    feature: Stack navigation 추가 및 타이핑

--- front/.eslintrc.js
+++ front/.eslintrc.js
@@ -1,4 +1,7 @@
 module.exports = {
   root: true,
-  extends: '@react-native',
+  extends: ['@react-native', '@tsconfig/react-native/tsconfig.json'],
+  rules: {
+    'no-unused-vars': ['warn', {varsIgnorePattern: 'StyleSheet'}],
+  },
 };
---
+++ front/src/screens/AuthHomeScreen.tsx
@@ -0,0 +1,26 @@
+import {StackScreenProps} from '@react-navigation/stack';
+import {Button, SafeAreaView, View} from 'react-native';
+import {AuthStackParamList} from '../navigation/AuthStackNavigator';
+import {authNavigations} from '../constants';
+
+type AuthHomeScreenProps = StackScreenProps<
+  AuthStackParamList,
+  typeof authNavigations.AUTH_HOME
+>;
+
+function AuthHomeScreen({navigation}: AuthHomeScreenProps) {
+  return (
+    <SafeAreaView>
+      <View>
+        <Button
+          title="로그인화면으로 이동"
+          onPress={() => navigation.navigate(authNavigations.LOGIN)}
+        />
+      </View>
+    </SafeAreaView>
+  );
+}
+
+// const styles = StyleSheet.create({});
+
+export default AuthHomeScreen;
